34acb80abee42574f4274d45dcffa8bf78f6a359
Dwi Andika <dwi.andika@kbru.co.id>
2024-11-03 23:11:04 +0700

    fix navbar

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -2,17 +2,15 @@
   <v-layout>
     <v-app-bar
       :color="appColor"
-      style="backdrop-filter: blur(15px);"
-      elevation="0"
-    >
+      style="backdrop-filter: blur(15px)"
+      :class="isActive ? 'app-bar__active' : ''"
+      elevation="0">
       <div class="app-bar__container">
-        <v-img 
+        <v-img
           src="assets/header.png"
           style="margin-left: -8px"
-          min-width="140" 
-          max-width="150" 
-          :class="isActive ? 'img-active' : 'img-nonactive'"
-          max-height="32">
+          class="app-bar__logo"
+          :class="isActive ? 'img-active' : 'img-nonactive'">
         </v-img>
         <v-spacer></v-spacer>
         <template v-if="$vuetify.display.mdAndUp">
@@ -22,13 +20,26 @@
           <v-btn elevation="0" class="btn-content">About</v-btn>
         </template>
         <v-spacer></v-spacer>
-        <v-btn @click="signIn()" style="border: 1px solid #fff3" class="btn-content">Sign in</v-btn>
+        <v-btn
+          @click="signIn()"
+          style="border: 1px solid #fff3"
+          :class="isActive ? 'btn-content__active' : 'btn-content'"
+          >Sign in</v-btn
+        >
         <template v-if="$vuetify.display.mdAndUp">
-          <v-btn @click="signUp()" variant="flat" color="#ff0000" class="btn-content__sign-up ml-4">Sign Up</v-btn>
+          <v-btn
+            @click="signUp()"
+            variant="flat"
+            color="#ff0000"
+            class="btn-content__sign-up ml-4"
+            >Sign Up</v-btn
+          >
         </template>
         <template v-if="!$vuetify.display.mdAndUp">
           <!-- <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon> -->
-          <div class="ml-4" style="border: 1px solid #fff3; border-radius: 4px; padding: 5px">
+          <div
+            class="ml-4"
+            style="border: 1px solid #fff3; border-radius: 4px; padding: 7.5px">
             <div class="side-bar" @click="onToggle()">
               <div class="menu-btn" :class="isActive ? 'active' : ''">
                 <span class="bar"></span>
@@ -40,72 +51,76 @@
         </template>
       </div>
     </v-app-bar>
-    <v-navigation-drawer
-      v-model="drawer"
-      temporary
-    >
-    <!-- :location="$vuetify.display.mobile ? 'top' : undefined" -->
-      <v-list
-        :items="items"
-      ></v-list>
+    <v-navigation-drawer v-model="drawer" temporary>
+      <!-- :location="$vuetify.display.mobile ? 'top' : undefined" -->
+      <v-list :items="items"></v-list>
     </v-navigation-drawer>
   </v-layout>
 </template>
 <script>
   export default {
-    name: 'HeaderComponent',
+    name: "HeaderComponent",
     data() {
       return {
         drawer: false,
         group: null,
-        appColor: 'rgb(255 255 255 / 0%)',
+        appColor: "rgb(255 255 255 / 0%)",
         isActive: false,
         items: [
           {
-            title: 'Foo',
-            value: 'foo',
+            title: "Foo",
+            value: "foo",
           },
           {
-            title: 'Bar',
-            value: 'bar',
+            title: "Bar",
+            value: "bar",
           },
           {
-            title: 'Fizz',
-            value: 'fizz',
+            title: "Fizz",
+            value: "fizz",
           },
           {
-            title: 'Buzz',
-            value: 'buzz',
+            title: "Buzz",
+            value: "buzz",
           },
         ],
-      }
+      };
     },
 
     watch: {
-      group () {
-        this.drawer = false
+      group() {
+        this.drawer = false;
+      },
+      drawer(val) {
+        if (val) {
+          this.isActive = true;
+          this.appColor = "white";
+        } else {
+          this.isActive = false;
+          this.appColor = "rgb(255 255 255 / 0%)";
+        }
       },
     },
     methods: {
       signUp() {
-        this.$router.push('/signup')
+        this.$router.push("/signup");
       },
       signIn() {
-        this.$router.push('/signin')
+        this.$router.push("/signin");
       },
       onToggle() {
-      this.isActive = !this.isActive
-      this.drawer = !this.drawer
-      if(!this.isActive) {
-        this.appColor = 'rgb(255 255 255 / 0%)'
-      } else {
-        this.appColor = 'white'
-      }
+        this.isActive = !this.isActive;
+        this.drawer = !this.drawer;
+        if (!this.isActive) {
+          this.appColor = "rgb(255 255 255 / 0%)";
+        } else {
+          this.appColor = "white";
+        }
+      },
     },
-    }
-  }
+  };
 </script>
 
 <style lang="scss" scoped>
-@import './style.scss'
+  @import "./style.scss";
 </style>
--- src/components/Hero/index.vue
+++ src/components/Hero/index.vue
@@ -2,68 +2,43 @@
   <div class="hero-container">
     <div class="hero-wrapper">
       <div class="top-hero">
-        <h1 class="hero-title">Sphere of Inspiration</h1>
+        <h1 class="hero-title">Blog Sphere</h1>
       </div>
-      <carousel :items-to-show="1" autoplay="2000">
-        <slide 
-          v-for="(slide, i) in slides"
-          :key="i"
-        >
-          <v-card style="width: 100%" flat height="400" color="transparent">
-            <v-row dense>
-              <v-col cols="8">
-                <v-sheet
-                  :color="colors[i]"
-                  height="400"
-                >
-                </v-sheet>
-              </v-col>
-              <v-col cols="4">
-                {{ slide }}
-              </v-col>
-            </v-row>
-          </v-card>
-        </slide>
-    
-        <template #addons>
-          <pagination />
-        </template>
-      </carousel>
+      <v-carousel
+        height="400"
+        show-arrows="hover"
+        cycle
+        hide-delimiter-background>
+        <v-carousel-item v-for="(slide, i) in slides" :key="i">
+          <v-sheet :color="colors[i]" height="100%">
+            <div class="d-flex fill-height justify-center align-center">
+              <div class="text-h2">{{ slide }} Slide</div>
+            </div>
+          </v-sheet>
+        </v-carousel-item>
+      </v-carousel>
     </div>
   </div>
 </template>
 
 <script>
-import 'vue3-carousel/dist/carousel.css'
-import { Carousel, Slide, Pagination } from 'vue3-carousel'
-export default {
-  data() {
-    return {
-      colors: [
-          'indigo',
-          'warning',
-          'pink darken-2',
-          'red lighten-1',
-          'deep-purple accent-4',
+  export default {
+    data() {
+      return {
+        colors: [
+          "indigo",
+          "warning",
+          "pink darken-2",
+          "red lighten-1",
+          "deep-purple accent-4",
         ],
-        slides: [
-          'First',
-          'Second',
-          'Third',
-          'Fourth',
-          'Fifth',
-        ],
-    }
-  },
-  components: {
-    Carousel,
-    Slide,
-    Pagination
-  },
-  name: 'HeroContainer'
-}
+        slides: ["First", "Second", "Third", "Fourth", "Fifth"],
+      };
+    },
+    name: "HeroContainer",
+  };
 </script>
 
 <style lang="scss" scoped>
-@import './style.scss'
+  @import "./style.scss";
 </style>
