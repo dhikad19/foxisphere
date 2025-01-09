59557a02a4a1ddefc920899831be88ca25ceef78
dikad911 <dwi.andika@kbru.co.id>
2025-01-09 17:02:15 +0700

    adjust UI

---
+++ src/components/Comment/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="comment-container">
+    <div class="comment-wrapper">
+      <div class="comment-content" @click="active = !active">
+        <p style="" v-if="!active">
+          Tambahkan Komentar
+        </p>
+        <v-expand-transition>
+          <v-card v-show="active" variant="flat">
+            <p>aktif</p>
+            <p>aktif</p>
+            <p>aktif</p>
+            <p>aktif</p>
+            <p>aktif</p>
+            <p>aktif</p>
+            <p>aktif</p>
+            <p>aktif</p>
+            <p>aktif</p>
+          </v-card>
+        </v-expand-transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommentComponent',
+  data() {
+    return {
+      active: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@use './style.scss'
+</style>
--- src/components/Post/index.vue
+++ src/components/Post/index.vue
@@ -18,49 +18,54 @@
         </div>
       </div>
       <div class="content">
-        <h1 class="post-title">
-          Revolusi Dunia Game dengan VR Revolusi Dunia Game dengan VR Revolusi 
-          Dunia Game dengan VR Revolusi Dunia Game dengan VR
-        </h1>
-        <p class="post-subtitle">
-          Pengalaman Bermain Game yang Tidak 
-          Terbatas dengan Virtual Reality
-        </p>
-        <p>
-          Pengalaman Bermain Game yang Tidak Terbatas dengan Virtual Reality
-  
-          Virtual Reality (VR) telah membuka babak baru dalam dunia permainan, mengubah cara kita berinteraksi dengan dunia digital. Sebelum VR, gaming sering kali terbatas pada layar datar dan kontroler konvensional. Kini, dengan hadirnya teknologi VR, pengalaman bermain game telah mengalami revolusi besar yang membawa para pemain ke dalam dunia virtual yang jauh lebih imersif dan realistis.
-  
-          Apa Itu VR dan Bagaimana Cara Kerjanya?
-          Virtual Reality adalah teknologi yang memungkinkan pengguna untuk memasuki lingkungan 3D digital melalui headset VR. Headset ini dilengkapi dengan sensor gerakan dan layar di dalamnya yang menampilkan dunia virtual. Ketika Anda bergerak atau mengarahkan kepala, dunia di dalam game juga bergerak sesuai dengan gerakan Anda. Hal ini memungkinkan pengalaman yang lebih menyeluruh dan mendalam.
-  
-          Dengan VR, pemain tidak hanya menjadi penonton, tetapi juga peserta aktif dalam setiap aksi yang terjadi dalam game. Dari perspektif orang pertama, Anda bisa merasakan sensasi seolah-olah berada langsung di dalam dunia game tersebut, memberikan pengalaman yang tidak bisa ditiru oleh game tradisional.
-  
-          Kenapa VR Mengubah Cara Kita Bermain?
-          1. Imersi yang Lebih Mendalam
-  
-          Salah satu daya tarik utama VR adalah tingkat imersi yang ditawarkannya. Dengan headset VR, pemain seakan berada di tengah-tengah aksi. Tidak ada lagi batasan layar yang membatasi pandangan Anda. Setiap sudut, detail, dan suara dalam game bisa dinikmati dengan intensitas yang jauh lebih tinggi.
-  
-          2. Interaksi yang Lebih Alami
-  
-          Berkat kontroler VR dan pelacakan gerakan tubuh, Anda bisa berinteraksi dengan dunia game secara langsung. Misalnya, Anda dapat memanipulasi objek, menyerang musuh, atau bahkan berlari menggunakan gerakan tubuh, bukan hanya dengan tombol-tombol di kontroler. Ini memberikan pengalaman yang lebih realistis dan menyenangkan.
-  
-          3. Pengalaman Sosial yang Lebih Nyata
-  
-          VR juga memungkinkan pengalaman bermain game secara sosial yang lebih nyata. Dalam game multiplayer berbasis VR, Anda dapat berinteraksi dengan pemain lain seolah-olah mereka berada di ruangan yang sama dengan Anda. Karakter avatar dalam game, meskipun hanya representasi digital, bisa terasa lebih hidup dan memungkinkan komunikasi lebih langsung.
-  
-          4. Menyentuh Berbagai Genre
-  
-          Dari aksi, petualangan, simulasi, hingga olahraga, VR memungkinkan pengembangan genre game yang lebih bervariasi. Anda dapat merasakan pengalaman mengemudi mobil balap, terjun payung di langit, atau menjelajahi dunia luar angkasa, semua dari kenyamanan rumah Anda. Keberagaman ini membuka peluang tak terbatas bagi pengembang game untuk menciptakan pengalaman yang lebih menarik dan inovatif.
-  
-          Tantangan dan Masa Depan VR dalam Dunia Game
-          Meski memiliki banyak keunggulan, teknologi VR masih menghadapi beberapa tantangan. Misalnya, harga perangkat yang relatif tinggi, kebutuhan ruang fisik yang lebih besar, dan potensi efek samping seperti mual atau ketegangan mata setelah penggunaan lama. Namun, inovasi terus berkembang, dan dengan semakin terjangkaunya harga perangkat serta peningkatan kualitas teknologi, VR diprediksi akan semakin terintegrasi dalam kehidupan gaming sehari-hari.
-  
-          Pada masa depan, kita mungkin akan melihat lebih banyak game yang menggabungkan VR dengan kecerdasan buatan (AI) dan teknologi lainnya, membawa kita ke pengalaman bermain game yang bahkan lebih imersif dan realistis.
-  
-          Kesimpulan
-          Virtual Reality telah merevolusi dunia game dengan membuka peluang baru untuk pengalaman bermain yang lebih mendalam, realistis, dan sosial. Walaupun masih ada tantangan yang perlu dihadapi, masa depan VR sangat cerah, dan para pemain game dapat menantikan lebih banyak inovasi yang akan membawa mereka ke dunia virtual yang semakin menakjubkan. Jika Anda ingin merasakan pengalaman bermain game yang benar-benar berbeda, kini adalah waktu yang tepat untuk mulai menjelajahi dunia VR!
-        </p>
+        <div class="content-data">
+          <h1 class="post-title">
+            Revolusi Dunia Game dengan VR Revolusi Dunia Game dengan VR Revolusi 
+            Dunia Game dengan VR Revolusi Dunia Game dengan VR
+          </h1>
+          <p class="post-subtitle">
+            Pengalaman Bermain Game yang Tidak 
+            Terbatas dengan Virtual Reality
+          </p>
+          <p>
+            Pengalaman Bermain Game yang Tidak Terbatas dengan Virtual Reality
+    
+            Virtual Reality (VR) telah membuka babak baru dalam dunia permainan, mengubah cara kita berinteraksi dengan dunia digital. Sebelum VR, gaming sering kali terbatas pada layar datar dan kontroler konvensional. Kini, dengan hadirnya teknologi VR, pengalaman bermain game telah mengalami revolusi besar yang membawa para pemain ke dalam dunia virtual yang jauh lebih imersif dan realistis.
+    
+            Apa Itu VR dan Bagaimana Cara Kerjanya?
+            Virtual Reality adalah teknologi yang memungkinkan pengguna untuk memasuki lingkungan 3D digital melalui headset VR. Headset ini dilengkapi dengan sensor gerakan dan layar di dalamnya yang menampilkan dunia virtual. Ketika Anda bergerak atau mengarahkan kepala, dunia di dalam game juga bergerak sesuai dengan gerakan Anda. Hal ini memungkinkan pengalaman yang lebih menyeluruh dan mendalam.
+    
+            Dengan VR, pemain tidak hanya menjadi penonton, tetapi juga peserta aktif dalam setiap aksi yang terjadi dalam game. Dari perspektif orang pertama, Anda bisa merasakan sensasi seolah-olah berada langsung di dalam dunia game tersebut, memberikan pengalaman yang tidak bisa ditiru oleh game tradisional.
+    
+            Kenapa VR Mengubah Cara Kita Bermain?
+            1. Imersi yang Lebih Mendalam
+    
+            Salah satu daya tarik utama VR adalah tingkat imersi yang ditawarkannya. Dengan headset VR, pemain seakan berada di tengah-tengah aksi. Tidak ada lagi batasan layar yang membatasi pandangan Anda. Setiap sudut, detail, dan suara dalam game bisa dinikmati dengan intensitas yang jauh lebih tinggi.
+    
+            2. Interaksi yang Lebih Alami
+    
+            Berkat kontroler VR dan pelacakan gerakan tubuh, Anda bisa berinteraksi dengan dunia game secara langsung. Misalnya, Anda dapat memanipulasi objek, menyerang musuh, atau bahkan berlari menggunakan gerakan tubuh, bukan hanya dengan tombol-tombol di kontroler. Ini memberikan pengalaman yang lebih realistis dan menyenangkan.
+    
+            3. Pengalaman Sosial yang Lebih Nyata
+    
+            VR juga memungkinkan pengalaman bermain game secara sosial yang lebih nyata. Dalam game multiplayer berbasis VR, Anda dapat berinteraksi dengan pemain lain seolah-olah mereka berada di ruangan yang sama dengan Anda. Karakter avatar dalam game, meskipun hanya representasi digital, bisa terasa lebih hidup dan memungkinkan komunikasi lebih langsung.
+    
+            4. Menyentuh Berbagai Genre
+    
+            Dari aksi, petualangan, simulasi, hingga olahraga, VR memungkinkan pengembangan genre game yang lebih bervariasi. Anda dapat merasakan pengalaman mengemudi mobil balap, terjun payung di langit, atau menjelajahi dunia luar angkasa, semua dari kenyamanan rumah Anda. Keberagaman ini membuka peluang tak terbatas bagi pengembang game untuk menciptakan pengalaman yang lebih menarik dan inovatif.
+    
+            Tantangan dan Masa Depan VR dalam Dunia Game
+            Meski memiliki banyak keunggulan, teknologi VR masih menghadapi beberapa tantangan. Misalnya, harga perangkat yang relatif tinggi, kebutuhan ruang fisik yang lebih besar, dan potensi efek samping seperti mual atau ketegangan mata setelah penggunaan lama. Namun, inovasi terus berkembang, dan dengan semakin terjangkaunya harga perangkat serta peningkatan kualitas teknologi, VR diprediksi akan semakin terintegrasi dalam kehidupan gaming sehari-hari.
+    
+            Pada masa depan, kita mungkin akan melihat lebih banyak game yang menggabungkan VR dengan kecerdasan buatan (AI) dan teknologi lainnya, membawa kita ke pengalaman bermain game yang bahkan lebih imersif dan realistis.
+    
+            Kesimpulan
+            Virtual Reality telah merevolusi dunia game dengan membuka peluang baru untuk pengalaman bermain yang lebih mendalam, realistis, dan sosial. Walaupun masih ada tantangan yang perlu dihadapi, masa depan VR sangat cerah, dan para pemain game dapat menantikan lebih banyak inovasi yang akan membawa mereka ke dunia virtual yang semakin menakjubkan. Jika Anda ingin merasakan pengalaman bermain game yang benar-benar berbeda, kini adalah waktu yang tepat untuk mulai menjelajahi dunia VR!
+          </p>
+        </div>
+        <div class="comment">
+          <Comment />
+        </div>
       </div>
       <div class="info" style="min-width: 400px">
 
@@ -70,8 +75,12 @@
 </template>
 
 <script>
+import Comment from '@/components/Comment/index.vue'
 export default {
-  name: 'PostComponent'
+  name: 'PostComponent',
+  components:  {
+    Comment
+  }
 }
 </script>
 
