3b3983979c674b05e05290633f142ac2dccaf8a8
dikad911 <dwi.andika@kbru.co.id>
2025-01-13 17:01:57 +0700

    update community content

--- src/components/Community/index.vue
+++ src/components/Community/index.vue
@@ -1,7 +1,152 @@
 <template>
   <div class="community-container">
     <div class="community-wrapper">
-      
+      <div class="mr-12 community-sidebar__sticky">
+        <div style="margin-right: 10px;">
+          <div style="width: 100%;" v-for="item in 6" :key="item" >
+            <v-card color="green" width="100%" class="mb-10" height="300">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugiat in, reprehenderit unde tempora non, sed ducimus placeat atque eligendi aspernatur cum odit, nihil ex beatae dicta? Officiis necessitatibus iure aliquam perspiciatis vitae praesentium molestiae suscipit, id similique qui a!</p>
+            </v-card>
+          </div>
+        </div>
+      </div>
+      <div class="community-content">
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+        <h1>vtest aja test aja test aja test aja test aja test 
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja test aja test aja test aja test
+          aja test aja test aja test aja test aja test aja 
+          test aja test aja 
+          </h1>
+      </div>
+      <div class="ml-12 community-sidebar__sticky">
+        <div style="margin-right: 10px;">
+          <div style="width: 100%; " v-for="item in 6" :key="item" >
+            <v-card color="green" width="100%" class="mb-10" height="300">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugiat in, reprehenderit unde tempora non, sed ducimus placeat atque eligendi aspernatur cum odit, nihil ex beatae dicta? Officiis necessitatibus iure aliquam perspiciatis vitae praesentium molestiae suscipit, id similique qui a!</p>
+            </v-card>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
