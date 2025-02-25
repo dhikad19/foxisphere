c58e3cf08630d7a9a1edc7f56c597e3b579a06ed
dikad911 <dwi.andika@kbru.co.id>
2025-02-25 17:08:24 +0700

    Add article component

---
+++ src/components/Articles/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="article-container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+  @use './style.scss';
+</style>
