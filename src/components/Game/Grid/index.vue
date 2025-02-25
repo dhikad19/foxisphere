53acd26b66a8307b9596a54a8f2f8377238e6347
dikad911 <dwi.andika@kbru.co.id>
2025-02-25 17:07:53 +0700

    Add game component and UI

---
+++ src/components/Game/Grid/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="game-grid-container">
+    <h1 class="game-title">Game Populer</h1>
+    <v-row dense>
+      <v-col cols="12" md="3">
+        <div class="game-card">
+          <v-card class="mx-auto" color="#f0f0f0" flat>
+            <v-img
+              class="game-image align-end"
+              height="120"
+              src="https://res.cloudinary.com/dqaylwrrx/image/upload/v1739259649/Games/star-rail_esjixp.png"
+              contain
+            >
+            </v-img>
+            
+          </v-card>
+          <div class="image-title">
+            <p>
+              Honkai: Star Rail
+            </p>
+          </div>
+        </div>
+      </v-col>
+      <v-col cols="12" md="3">
+        <div class="game-card">
+          <v-card class="mx-auto" color="#f0f0f0" flat>
+            <v-img
+              class="game-image align-end"
+              height="120"
+              src="https://res.cloudinary.com/dqaylwrrx/image/upload/v1739259775/Games/zzz_rceyrv.png"
+              contain
+            >
+              
+            </v-img>
+            
+          </v-card>
+          <div class="image-title">
+            <p>
+              Zenless Zone Zero
+            </p>
+          </div>
+        </div>
+      </v-col>
+      <v-col cols="12" md="3">
+        <div class="game-card">
+          <v-card class="mx-auto" color="#f0f0f0" flat>
+            <v-img
+              class="game-image align-end"
+              height="120"
+              src="https://res.cloudinary.com/dqaylwrrx/image/upload/v1739259648/Games/genhin-impact_tvqxbi.png"
+              contain
+            >
+              
+            </v-img>
+            
+          </v-card>
+          <div class="image-title">
+            <p>
+              Genshin Impact
+            </p>
+          </div>
+        </div>
+      </v-col>
+      <v-col cols="12" md="3">
+        <div class="game-card">
+          <v-card class="mx-auto" color="#f0f0f0" flat>
+            <v-img
+              class="game-image align-end"
+              height="120"
+              src="https://res.cloudinary.com/dqaylwrrx/image/upload/v1740466847/Games/wuthering-waves_gpltcq.png"
+              contain
+            >
+              
+            </v-img>
+            
+          </v-card>
+          <div class="image-title">
+            <p>
+              Wuthering Waves
+            </p>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameGridComponents'
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../style.scss';
+</style>
---
+++ src/components/Game/List/index.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="game-list-container">
+    <h1 class="game-title">Game Lainnya</h1>
+    <v-row dense>
+      <v-col cols="12" md="3" v-for="(item, i) in gameList" :key="i">
+        <div class="game-card">
+          <v-card class="mx-auto" color="#f0f0f0" flat>
+            <v-img
+              class="game-image align-end"
+              :style="item.name == 'AFK Journey' ? 'filter: grayscale(100%) brightness(0)' : ''"
+              height="120"
+              :src="item.image"
+              contain
+            >
+            </v-img>
+            
+          </v-card>
+          <div class="image-title">
+            <p>
+              {{item.name}}
+            </p>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameListComponent',
+  data() {
+    return {
+      gameList: [
+        {
+          id: '1',
+          image: 'https://res.cloudinary.com/dqaylwrrx/image/upload/v1740473419/Games/reverse-1999_voa7td.png',
+          name: 'Reverse: 1999'
+        },
+        {
+          id: '2',
+          image: 'https://res.cloudinary.com/dqaylwrrx/image/upload/v1740469634/Games/arknights_oz6wzl.png',
+          name: 'Arknights'
+        },
+        {
+          id: '3',
+          image: 'https://res.cloudinary.com/dqaylwrrx/image/upload/v1740469629/Games/epic-seven_psohrd.png',
+          name: 'Epic Seven'
+        },
+        {
+          id: '4',
+          image: 'https://res.cloudinary.com/dqaylwrrx/image/upload/v1740466847/Games/wuthering-waves_gpltcq.png',
+          name: 'Wuthering Waves'
+        },
+        {
+          id: '5',
+          image: 'https://res.cloudinary.com/dqaylwrrx/image/upload/v1740469635/Games/honkai-impact_tgn56j.png',
+          name: 'Honkai Impact'
+        },
+        {
+          id: '6',
+          image: 'https://res.cloudinary.com/dqaylwrrx/image/upload/v1739259775/Games/zzz_rceyrv.png',
+          name: 'Zenless Zone Zero'
+        },
+        {
+          id: '7',
+          image: 'https://res.cloudinary.com/dqaylwrrx/image/upload/v1739259648/Games/genhin-impact_tvqxbi.png',
+          name: 'Genshin Impact'
+        },
+        {
+          id: '8',
+          image: 'https://res.cloudinary.com/dqaylwrrx/image/upload/v1739259649/Games/star-rail_esjixp.png',
+          name: 'Honkai: Star Rail'
+        },
+        {
+          id: '9',
+          image: 'https://res.cloudinary.com/dqaylwrrx/image/upload/v1740469629/Games/summoners-war_f5u2md.png',
+          name: 'Summoners War'
+        },
+        {
+          id: '10',
+          image: 'https://res.cloudinary.com/dqaylwrrx/image/upload/v1740469636/Games/Group_63_whjhdt.png',
+          name: 'Guardian Tales'
+        },
+        {
+          id: '11',
+          image: 'https://res.cloudinary.com/dqaylwrrx/image/upload/v1740469634/Games/brown-dust2_aceotn.png',
+          name: 'Brown Dust 2'
+        },
+        {
+          id: '12',
+          image: 'https://res.cloudinary.com/dqaylwrrx/image/upload/v1740473418/Games/afk-journey_pivr1a.png',
+          name: 'AFK Journey'
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../style.scss';
+</style>
