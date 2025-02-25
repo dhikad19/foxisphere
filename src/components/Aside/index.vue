468328da08217e0c87961d10616e2c6fea5a8d7b
dikad911 <dwi.andika@kbru.co.id>
2025-02-25 17:09:10 +0700

    Add aside component

---
+++ src/components/Aside/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="aside-container ml-8">
+    <div class="aside-wrapper">
+
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use './style.scss';
+</style>
+
+<script>
+export default {
+  name: 'AsideComponent'
+}
+</script>
