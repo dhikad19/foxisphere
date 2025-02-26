214af3a153108662a8b03d7d1291fa8058a716e6
dikad911 <dwi.andika@kbru.co.id>
2025-02-26 17:01:55 +0700

    Add support page

---
+++ src/components/Support/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="support-container">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SupportComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+@use './style.scss';
+</style>
