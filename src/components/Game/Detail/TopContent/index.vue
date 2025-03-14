e79372a75f400fe29591b1ab5244fe2b4af66500
dikad911 <dwi.andika@kbru.co.id>
2025-03-14 14:26:48 +0700

    Add top content image

--- src/components/Game/Detail/TopContent/index.vue
+++ src/components/Game/Detail/TopContent/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="top-container">
-    
+    <v-img src="https://res.cloudinary.com/dqaylwrrx/image/upload/v1741934673/Games%20Banner/hsr-banner_idxcz8.webp"></v-img>
   </div>
 </template>
 
