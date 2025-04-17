7cf4ed6650203535ff7e291e864a1df512fc51d8
dikad911 <dwi.andika@kbru.co.id>
2025-04-17 16:45:11 +0700

    Adjust tier list UI

--- src/components/Game/Detail/Content/TierList/index.vue
+++ src/components/Game/Detail/Content/TierList/index.vue
@@ -5,7 +5,7 @@
 
       </div>
       <div class="tierlist-content" >
-        <div>
+        <div class="tierlist-desktop">
           <v-row dense class="mb-6 mt-6" style="margin-left: 74px; margin-right: 0px">
             <v-col
               v-for="(item, i) in categoriesList"
@@ -14,7 +14,6 @@
               class="text-center"
             >
               <v-card :color="item.backgroundColor" height="50" flat outlined style="border-radius: 0px;" class="d-flex align-center justify-center">
-                <v-icon :color="item.color" size="20" class="mr-2">{{item.icon}}</v-icon>
                 <span :style="{'color': item.color}" class="categories-title">{{ item.name }}</span>
               </v-card>
             </v-col>
@@ -59,6 +58,13 @@
                             <div style="background: linear-gradient(180deg, #885550,#c9a36a 53%);">
                               <v-img :src="character.image" />
                             </div>
+                            <div style="background: #c9a36a; margin-top: -15px; padding: 5px">
+                              <p style="color: #000; line-height: normal; font-weight: 500; text-transform: capitalize; font-size: 13px" class="text-center">{{character.name}}</p>
+                            </div>
+                            <div class="mt-2" style="color: #d1d1d1; font-size: 12px; line-height: 16px; text-transform: uppercase; font-weight: bold">
+                              <span style="margin: 0px; padding: 0px;">{{character.path.name}}, </span>
+                              <span :style="{color: character.combatType.color}" style="margin: 0px; padding: 0px;">{{character.combatType.name}}</span>
+                            </div>
                           </v-card>
                         </template>
                         <v-card color="#000" class="pa-3" width="100%">
@@ -78,14 +84,64 @@
                           </v-row>
                         </v-card>
                       </v-menu>
-                      <v-card-title>{{ character.name }}</v-card-title>
-                      <v-card-subtitle>{{ character.combatType }}</v-card-subtitle>
                     </v-col>
                   </v-row>
                 </v-card>
               </div>
             </div>
           </div>
+        </div>
+        <div class="tierlist-mobile">
+          <div v-for="(tier, i) in tiers" :key="i" class="mb-6">
+            <!-- Tier title -->
+            <v-card 
+              :color="tier.color" 
+              class="d-flex align-center justify-center" 
+              flat
+              style="border-radius: 0px;"
+              height="100%"
+              min-width="50"
+            >
+              <p class="tier-ranks">{{tier.name}}</p>
+            </v-card>
+            
+            <div v-for="(category, j) in categoriesList" :key="j">
+              <div v-if="filteredCharactersMobile(tier.name, category.name).length">
+                <h3 class="text-subtitle-2 font-weight-medium mb-2 mt-2 text-center" :style="{ color: category.color }">
+                  <!-- <v-icon size="18" left :color="category.color">{{ category.icon }}</v-icon> -->
+                  {{ category.name.toUpperCase() }}
+                </h3>
+                <v-row dense>
+                  <v-col
+                    v-for="character in filteredCharactersMobile(tier.name, category.name)"
+                    :key="character.id + tier.name"
+                    cols="3"
+                  >
+                    <v-card flat class="pa-1" color="transparent" style="cursor: pointer">
+                      <div style="background: linear-gradient(180deg, #885550,#c9a36a 53%);">
+                        <v-img aspect-ratio="2/1" :src="character.image" >
+                          <div class="d-flex justify-space-between" v-if="character.new">
+                            <div></div>
+                            <div style="background-color: #ed343e; color: white; padding: 1px 5px 1px 5px">
+                              <p style="font-size: 13px; font-weight: bold">NEW</p>
+                            </div>
+                          </div>
+                        </v-img>
+                      </div>
+                      <div style="background: #c9a36a; margin-top: -15px; padding: 5px">
+                        <p style="color: #000; line-height: normal; font-weight: 500; text-transform: capitalize; font-size: 13px" class="text-center">{{character.name}}</p>
+                      </div>
+                      <div class="mt-2" style="color: #d1d1d1; font-size: 12px; line-height: 16px; text-transform: uppercase; font-weight: bold">
+                        <span style="margin: 0px; padding: 0px;">{{character.path.name}}, </span>
+                        <span :style="{color: character.combatType.color}" style="margin: 0px; padding: 0px;">{{character.combatType.name}}</span>
+                      </div>
+                    </v-card>
+                  </v-col>
+                </v-row>
+              </div>
+            </div>
+          </div>
+          
         </div>
       </div>
     </div>
@@ -102,7 +158,84 @@ export default {
           name: 'mydei',
           new: true,
           combatType: {
-            name: 'imaginary',
+            name: 'quantum',
+            color: '#736ae6',
+            image: 'https://i.ibb.co.com/Swk1ZXzq/Type-Imaginary-2.png'
+          },
+          categories: 'dps',
+          rarity: '5',
+          path: {
+            name: 'abundance',
+            image: 'https://i.ibb.co.com/xtFZ6gxX/Path-Destruction-1.png'
+          },
+          image: 'https://i.ibb.co.com/N6thkzsM/argenti.png',
+          games: 'honkai-star-rail',
+          id: '1',
+          tier: 'SSS'
+        },
+        {
+          name: 'mydei',
+          new: false,
+          combatType: {
+            name: 'fire',
+            color: '#e83e3e',
+            image: 'https://i.ibb.co.com/Swk1ZXzq/Type-Imaginary-2.png'
+          },
+          categories: 'dps',
+          rarity: '5',
+          path: {
+            name: 'nihility',
+            image: 'https://i.ibb.co.com/xtFZ6gxX/Path-Destruction-1.png'
+          },
+          image: 'https://i.ibb.co.com/N6thkzsM/argenti.png',
+          games: 'honkai-star-rail',
+          id: '1',
+          tier: 'SSS'
+        },
+        {
+          name: 'mydei',
+          new: false,
+          combatType: {
+            name: 'physical',
+            color: '#acabab',
+            image: 'https://i.ibb.co.com/Swk1ZXzq/Type-Imaginary-2.png'
+          },
+          categories: 'dps',
+          rarity: '5',
+          path: {
+            name: 'destruction',
+            image: 'https://i.ibb.co.com/xtFZ6gxX/Path-Destruction-1.png'
+          },
+          image: 'https://i.ibb.co.com/N6thkzsM/argenti.png',
+          games: 'honkai-star-rail',
+          id: '1',
+          tier: 'SSS'
+        },
+        {
+          name: 'mydei',
+          new: false,
+          combatType: {
+            name: 'ice',
+            color: '#1fb6d1',
+            image: 'https://i.ibb.co.com/Swk1ZXzq/Type-Imaginary-2.png'
+          },
+          categories: 'dps',
+          rarity: '5',
+          path: {
+            name: 'destruction',
+            image: 'https://i.ibb.co.com/xtFZ6gxX/Path-Destruction-1.png'
+          },
+          image: 'https://i.ibb.co.com/N6thkzsM/argenti.png',
+          games: 'honkai-star-rail',
+          id: '1',
+          tier: 'SSS'
+        },
+        {
+          name: 'mydei',
+          new: false,
+          combatType: {
+            name: 'wind',
+            color: '#3cc088',
             image: 'https://i.ibb.co.com/Swk1ZXzq/Type-Imaginary-2.png'
           },
           categories: 'dps',
@@ -118,9 +251,10 @@ export default {
         },
         {
           name: 'tribbie',
-          new: true,
+          new: false,
           combatType: {
             name: 'imaginary',
+            color: '#d6c146',
             image: 'https://i.ibb.co.com/Swk1ZXzq/Type-Imaginary-2.png'
           },
           categories: 'support',
@@ -139,6 +273,7 @@ export default {
           new: false,
           combatType: {
             name: 'imaginary',
+            color: '#d6c146',
             image: 'https://i.ibb.co.com/Swk1ZXzq/Type-Imaginary-2.png'
           },
           categories: 'dps',
@@ -157,6 +292,7 @@ export default {
           new: false,
           combatType: {
             name: 'imaginary',
+            color: '#d6c146',
             image: 'https://i.ibb.co.com/Swk1ZXzq/Type-Imaginary-2.png'
           },
           categories: 'dps',
@@ -175,6 +311,7 @@ export default {
           new: false,
           combatType: {
             name: 'imaginary',
+            color: '#d6c146',
             image: 'https://i.ibb.co.com/Swk1ZXzq/Type-Imaginary-2.png'
           },
           categories: 'healer',
@@ -193,6 +330,7 @@ export default {
           new: false,
           combatType: {
             name: 'imaginary',
+            color: '#d6c146',
             image: 'https://i.ibb.co.com/Swk1ZXzq/Type-Imaginary-2.png'
           },
           categories: 'shielder',
@@ -211,6 +349,7 @@ export default {
           new: false,
           combatType: {
             name: 'imaginary',
+            color: '#d6c146',
             image: 'https://i.ibb.co.com/Swk1ZXzq/Type-Imaginary-2.png'
           },
           categories: 'healer',
@@ -229,6 +368,7 @@ export default {
           new: false,
           combatType: {
             name: 'imaginary',
+            color: '#d6c146',
             image: 'https://i.ibb.co.com/Swk1ZXzq/Type-Imaginary-2.png'
           },
           categories: 'healer',
@@ -284,6 +424,11 @@ export default {
     filteredCharacters(tier, category) {
       return this.tierList.filter(character => character.tier === tier && character.categories === category);
     },
+    filteredCharactersMobile(tier, category) {
+      return this.tierList.filter(
+        char => char.tier === tier && char.categories === category
+      );
+    }
   }
 }
 </script>
